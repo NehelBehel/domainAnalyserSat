fc2fe3d1c157d24ddb946ce6527156b20b1a0b04
Neil <surface@DESKTOP-LL2CN0H>
2026-07-24 16:37:50 +1000

    add pipeline stepper UI
     Replaced top nav buttons with a new pipeline stepper for session workflow. Updated event handlers to manage sidebar active states and pipeline stepper visibility. Cleaned up unused top nav event handlers.

--- domainAnalyserSat/shellWindow.xaml.cs
+++ domainAnalyserSat/shellWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Printing;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -9,6 +10,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Windows.Input;
 
 namespace domainAnalyserSat
 {
@@ -29,16 +31,65 @@ namespace domainAnalyserSat
 
 
 
+        //Begin a new session, shows the import screen and updates the progress pipleline at the top 
+
+
+        private void btnNewAnalysis_Click(object sender, RoutedEventArgs e)
+        {
+            //import view is shown add later 
+            pipelineStepper.Visibility = Visibility.Visible;
+            clearSideBarActive();
+            
+
+        }
+
+        private void stepImport_Click(object sender, MouseButtonEventArgs e ) //
+        {
+            //import view 
+        }
+
         private void btnNavDashboard_Click(object sender, RoutedEventArgs e)
         {
             contentHost.Content = new workSpaceOV();
-            setActiveNav(btnNavDashboard); //set the dashboard nav item to active)
-
+            pipelineStepper.Visibility = Visibility.Collapsed; //
+            setActiveNav(btnNavDashboard);
         }
-           private void btnNavWatchlist_Click(object sender, RoutedEventArgs e) { }
 
-           private void btnNavSession_Click(object sender, RoutedEventArgs e) { }
-           private void btnNewAnalysis_Click(object sender, RoutedEventArgs e) { }
+
+        private void btnNavWatchlist_Click(object sender, RoutedEventArgs e)
+        {
+            pipelineStepper.Visibility = Visibility.Collapsed;
+            setActiveNav(btnNavWatchlist);
+            // contentHost.Content = new watchlistView();   when built
+        }
+
+        private void btnNavSession_Click(object sender, RoutedEventArgs e)
+        {
+            pipelineStepper.Visibility = Visibility.Collapsed;
+            setActiveNav(btnNavSession);
+            // contentHost.Content = new sessionView();      // when built
+        }
+
+
+
+
+
+        private void clearSideBarActive()
+        {
+            //Reset sidebar style back to defaults 
+
+
+            foreach (var btn in new[] {btnNavDashboard, btnNavWatchlist, btnNavSession })
+            {
+                btn.Style = (Style)FindResource("sidebarNavBtn");
+            }
+        }
+
+
+
+      
+        
+         
            private void btnTopImport_Click(object sender, RoutedEventArgs e) { }
            private void btnTopAnalysis_Click(object sender, RoutedEventArgs e) { }
            private void btnTopValidate_Click(object sender, RoutedEventArgs e) { }
@@ -55,9 +106,9 @@ namespace domainAnalyserSat
 
             //reset all navigation items to default state
             //mark the curr active nav item 
-            btnNavDashboard.Style = (Style)FindResource("sidebarNavBtnActive");
-            btnNavWatchlist.Style = (Style)FindResource("sidebarNavBtnActive");
-            btnNavSession.Style = (Style)FindResource("sidebarNavBtnActive");
+            btnNavDashboard.Style = (Style)FindResource("sidebarNavBtn");
+            btnNavWatchlist.Style = (Style)FindResource("sidebarNavBtn");
+            btnNavSession.Style = (Style)FindResource("sidebarNavBtn");
             active.Style = (Style)FindResource("sidebarNavBtnActive");
             //(Style)FindResource("sidebarNavBtnActive");
 
