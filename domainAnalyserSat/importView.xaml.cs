afe0cfa0c0429bfb373861f21a7cf3bea8527d42
Neil <surface@DESKTOP-LL2CN0H>
2026-07-27 22:40:01 +1000

    added basic file imports

--- domainAnalyserSat/importView.xaml.cs
+++ domainAnalyserSat/importView.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -14,12 +16,92 @@ using System.Windows.Shapes;
 namespace domainAnalyserSat
 {
    
+   
+
     public partial class importView : UserControl
     {
+        public string Data { get; set; }
         public importView()
         {
             InitializeComponent();
         }
+
+        //Allows the user to browse files and make selection 
+        private void btnBrowse_Click(object sender, RoutedEventArgs e)
+        {
+
+            //Open file dialogue box 
+            OpenFileDialog dialog = new OpenFileDialog
+            {
+                Filter = "Domain lists (*.csv;*.txt)|*.csv;*.txt|All files (*.*)|*.*" //claude generated this filter 
+            };
+           
+            if (dialog.ShowDialog() == true)
+            {
+                setSelectedFile(dialog.FileName);
+            }
+
+
+        }
+
+       //Show the chosen file
+       //Put in its own method as it needs to be called by both file input locatiions (drag + manual selection)
+       //TO DO: Add reading/ parsing of the file 
+        private void setSelectedFile(string path)
+        {
+            txtSelectedFile.Text = System.IO.Path.GetFileName(path); //take the full path and return file name 
+
+
+
+
+        }
+
+
+        //Allows the user to drag filees into desingated zone 
+
+        private void importField_DragOver(object sender, DragEventArgs e)
+        {
+           
+
+            e.Effects = e.Data.GetDataPresent(DataFormats.FileDrop) //if user is dragging file, set to true, otherwise false 
+                ? DragDropEffects.Copy //true- windows will show + copy cursor signalling to the user 
+                : DragDropEffects.Move; //no drop cursor is shwon 
+
+        }
+        
+        
+        private void importField_Drop(object sender, DragEventArgs e)
+        {
+
+            if (e.Data.GetData(DataFormats.FileDrop) is string[] files && files.Length >0)
+            {
+                //if data returns string, test pass,  result is stored into var files 
+                //if null or not string, test returns false 
+
+
+
+
+
+
+                    //grab file path
+                    string selectedFilePath = files[0];
+
+                    //Display 
+                    MessageBox.Show(System.IO.Path.GetFileName(selectedFilePath));
+
+                
+            }
+
+
+
+        }
+
+
+
+
+
+
+
 
         
     }
